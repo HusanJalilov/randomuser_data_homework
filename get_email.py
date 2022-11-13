d078454960b392850851dbace27bb6fd4ebf7927
coder_uz <husanjalilov00777@gmil.com>
2022-11-13 21:52:52 +0500

    02

--- get_email.py
+++ get_email.py
@@ -10,7 +10,7 @@ def get_email(data:dict) -> list:
     Returns:
         list: users email
     """
-    s=data[results]
+    s=data['results']
     m=[]
     for i in s:
         m.append(i['email'])
