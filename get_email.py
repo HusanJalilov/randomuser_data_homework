20f76a427590d11a8e8235e54f3712d5acfab523
coder_uz <husanjalilov00777@gmil.com>
2022-11-13 19:50:46 +0500

    02

--- get_email.py
+++ get_email.py
@@ -9,13 +9,12 @@ def get_email(filename:str) -> dict:
         filename(str): file name
     Returns:
         dict: JSON data
-    """
-    
+    """    
 
-    s=json.loads(filename)['results']
-    d=''
-    for i in range(len(s)):
-        d+=s[i].get('email')+'\n'
-    return d[:-1]
-data=open("randomuser_data.json").read()
-print(get_data(data))
+#     s=json.loads(filename)['results']
+#     d=''
+#     for i in range(len(s)):
+#         d+=s[i].get('email')+'\n'
+#     return d[:-1]
+# data=open("randomuser_data.json").read()
+# print(get_data(data))
